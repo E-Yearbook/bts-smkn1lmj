f39054fb05a8e4e938b391caf5e7532555ca4c78
dream22301 <brilliantprincoekananda2009@gmail.com>
2026-03-07 13:37:13 +0700

    membuat migration

---
+++ database/migrations/2026_03_07_061028_create_year_covers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('year_covers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('year');
+            $table->string('cover_path');
+            $table->string('youtube_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('year_covers');
+    }
+};
---
+++ database/migrations/2026_03_07_061234_create_table_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('table_books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('book_category_id')->nullable()->constrained("book_categories")->onDelete('cascade');
+            $table->foreignId('year_cover_id')->nullable()->constrained("year_covers")->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained("users")->onDelete('cascade');
+            $table->string('name');
+            $table->string('publisher');
+            $table->string('book_cover')->nullable();
+            $table->string('book_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_books');
+    }
+};
